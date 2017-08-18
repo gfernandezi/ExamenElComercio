326498ce01510f516e4e4ebc52e1c5076cf41ddb
gfernandezi <giancarlo.fernandez@gmail.com>
2017-08-18 13:41:27 -0500

    actualizandoo

--- ExamenMetrica/Entidades/OrdenDePago.cs
+++ ExamenMetrica/Entidades/OrdenDePago.cs
@@ -11,7 +11,7 @@ namespace ExamenMetrica.Entidades
         [Key]
         public int Codigo { get; set; }
         public double monto { get; set; }
-        public double moneda { get; set; }
+        public string moneda { get; set; }
         public string estado { get; set; }
         public DateTime fechaPago { get; set; }
         public int codigoSucursal { get; set; }
